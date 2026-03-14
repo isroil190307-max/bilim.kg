c29131368d8d897d349762af861fe0eba1f7b4f3
isroil190307-max <isroil190307@gmail.com>
2026-03-15 00:31:26 +0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,9 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('bilim-store').then((cache) => cache.addAll([
-      './',
-      './index.html',
-      './matematika.html',
-      './geografiya.html',
-      './informatika.html',
-      './library.html',
-      './geo-lesson1.html',
-      './geo-test1.html',
-      './info-lesson1.html',
-      './info-test1.html',
-      './style.css',
-      './manifest.json'
-    ]))
-  );
-});
-
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    // Алгач интернеттен жаңы версиясын сурайт
+    fetch(e.request).catch(() => {
+      // Эгер интернет жок болсо гана эстутумдан (кэштен) алат
+      return caches.match(e.request);
+    })
   );
 });
